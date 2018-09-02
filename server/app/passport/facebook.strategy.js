3d7d8cb2daf1bc134642528effe743271e4f7ae0
Mustafa Ahmed <imustafa97@outlook.com>
2018-09-02 10:17:23 +0200

    Facebook Email Fix

--- server/app/passport/facebook.strategy.js
+++ server/app/passport/facebook.strategy.js
@@ -6,9 +6,18 @@ passport.use(
   new FacebookStrategy({
     clientID: process.env.facebook_clientID,
     clientSecret: process.env.facebook_clientSecret,
-    callbackURL: 'https://www.eyeou.net/auth/facebook/redirect'
+    // callbackURL: 'https://www.eyeou.net/auth/facebook/redirect',
+    callbackURL: 'http://localhost:8080/auth/facebook/redirect',
+    profileFields: ['emails']
   }, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ facebookID: profile.id })
+    let query = {
+      email: profile.emails ? profile.emails[0].value : undefined,
+      facebookID: profile.id
+    }
+    Object.keys(query).forEach(key => query[key] === undefined && delete query[key]);
+    if (query.email && query.facebookID) delete query.facebookID;
+
+    User.findOne(query)
       .exec((err, currentUser) => {
         if (err) console.log(err)
         if (currentUser) {
@@ -17,7 +26,8 @@ passport.use(
           new User({
             fullName: profile.displayName,
             facebookID: profile.id,
-            verified: true
+            verified: true,
+            email: profile.emails ? profile.emails[0].value : null
           }).save((err, user) => {
             if (err) console.log(err)
             done(null, user)
